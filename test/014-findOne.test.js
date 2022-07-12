9fc55218d9669829f8d729a115a9525eee3ccc27
Miroslav Popov <info@forexsb.com>
2022-07-12 09:38:13 +0300

    queryOne accepts an empty query

--- test/014-findOne.test.js
+++ test/014-findOne.test.js
@@ -1,39 +1,47 @@
 'use strict'
 
-const {strictEqual} = require('assert')
+const {strictEqual}  = require('assert')
 const {describe, it} = require('@popovmp/mocha-tiny')
+const {getDb}        = require('../index.js')
 
-const {getDb} = require('../index.js')
-const db  = getDb()
+const db = getDb()
 
 db.insert({planet: 'Mars',    system: 'solar',  inhabited: false})
 db.insert({planet: 'Earth',   system: 'solar',  inhabited: true })
 db.insert({planet: 'Jupiter', system: 'solar',  inhabited: false})
 db.insert({planet: 'Omicron', system: 'micron', inhabited: true })
 
-describe('findOne tests', () => {
+describe('findOne', () => {
 
-	describe('findOne({})', () => {
+	describe('when called with an empty query', () => {
 		const doc = db.findOne({})
 
-		it('returns undefined', () => {
-			strictEqual(doc, undefined)
+		it('returns one of the db docs', () => {
+			strictEqual(typeof doc, 'object')
 		})
 	})
 
-	describe('findOne multiple matches', () => {
+	describe('whe matches multiple docs', () => {
 		const doc = db.findOne({system: 'solar'})
 
-		it('returns a doc', () => {
+		it('returns one of the matched docs', () => {
 			strictEqual(doc['system'], 'solar')
 		})
 	})
 
-	describe('exact match', () => {
+	describe('when matches a single doc', () => {
 		const doc = db.findOne({system: 'solar', inhabited: true})
 
-		it('returns correct doc', () => {
+		it('returns the correct doc', () => {
 			strictEqual(doc['planet'], 'Earth')
+		})
+	})
+
+	describe('when does not match a doc', () => {
+		const doc = db.findOne({system: 'sirius'})
+
+		it('returns `undefined`', () => {
+			strictEqual(doc, undefined)
 		})
 	})
 })
