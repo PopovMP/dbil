590e6b8e0bae323c71cffe57cbb707d39c45ba5d
Miroslav Popov <info@forexsb.com>
2024-06-08 15:01:05 +0300

    Code cleanup.

--- test/100-benchmark.test.js
+++ test/100-benchmark.test.js
@@ -1,20 +1,19 @@
 "use strict";
 
-const {strictEqual}  = require("assert");
-const {describe, it} = require("node:test");
-const {getDb}        = require("../index.js");
+const { ok          } = require("assert");
+const { describe, it} = require("node:test");
+const { getDb       } = require("../index.js");
 
 describe("benchmark", () => {
-    const db           = getDb(); // Make an in-memory DB
+    const db = getDb(); // Make an in-memory DB
     const countObjects = 1000;
 
     function validate(operation, timeStart, count) {
         const time = Date.now() - timeStart;
 
-        it(`${operation} ops/sec > 5000`, () => {
-            const opsPerSec = Math.round((1000 / time) * countObjects);
-            console.log(`        ${operation} ${count} docs for ${time}ms. ops/sec: ${opsPerSec}`);
-            strictEqual(opsPerSec > 5000, true);
+        const opsPerSec = Math.round((1000 / (time || 1)) * countObjects);
+        it(`${operation} ${count} docs for ${time}ms. ${opsPerSec} ops/sec`, () => {
+            ok(opsPerSec > 5000);
         });
     }
 
@@ -22,9 +21,8 @@ describe("benchmark", () => {
         const timeStart = Date.now();
         let   count     = 0;
 
-        for (let i = 0; i < countObjects; i += 1) {
+        for (let i = 0; i < countObjects; i += 1)
             count += db.insert({index: i, b: 42}) ? 1 : 0;
-        }
 
         validate("insert", timeStart, count);
     });
@@ -33,9 +31,8 @@ describe("benchmark", () => {
         const timeStart = Date.now();
         let   count     = 0;
 
-        for (let i = 0; i < countObjects; i += 1) {
+        for (let i = 0; i < countObjects; i += 1)
             count += db.find({index: i, b: {$gte: 42}}, {index: true}).length;
-        }
 
         validate("find", timeStart, count);
     });
@@ -44,9 +41,8 @@ describe("benchmark", () => {
         const timeStart = Date.now();
         let   count     = 0;
 
-        for (let i = 0; i < countObjects; i += 1) {
+        for (let i = 0; i < countObjects; i += 1)
             count += db.findOne({index: i, b: {$gte: 42}}, {index: true}) ? 1 : 0;
-        }
 
         validate("findOne", timeStart, count);
     });
@@ -57,20 +53,18 @@ describe("benchmark", () => {
 
         const ids = db.find({}, {_id: 1}).map(doc => doc._id);
 
-        for (const id of ids) {
+        for (const id of ids)
             count += db.findOne({_id: id}, {index: 1}) ? 1 : 0;
-        }
 
-        validate("findOne", timeStart, count);
+        validate("findOne by _id", timeStart, count);
     });
 
     describe("update", () => {
         const timeStart = Date.now();
         let   count     = 0;
 
-        for (let i = 0; i < countObjects; i += 1) {
+        for (let i = 0; i < countObjects; i += 1)
             count += db.update({index: i, b: {$gte: 42}}, {$set: {b: 13}}, {multi: false});
-        }
 
         validate("update", timeStart, count);
     });
@@ -79,9 +73,8 @@ describe("benchmark", () => {
         const timeStart = Date.now();
         let   count     = 0;
 
-        for (let i = 0; i < countObjects; i += 1) {
+        for (let i = 0; i < countObjects; i += 1)
             count += db.remove({index: i, b: {$lt: 42}});
-        }
 
         validate("remove", timeStart, count);
     });
